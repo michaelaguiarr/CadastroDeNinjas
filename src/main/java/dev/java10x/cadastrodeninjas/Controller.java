c63adee5d84614c16c4f3a47fc7f0d566c3dbdf1
michael.aguiar <michael.aguiar@rpe.tech>
2025-03-14 01:18:46 -0400

    Criado a controller [Aula2]

--- src/main/java/dev/java10x/cadastrodeninjas/Controller.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Controller.java
@@ -1,4 +1,16 @@
 package dev.java10x.cadastrodeninjas;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
 public class Controller {
+
+    @GetMapping("/boas-vindas")
+    public String boasVindas(){
+        return "Bem vindo ao Cadastro de Ninjas!";
+    }
+
 }
