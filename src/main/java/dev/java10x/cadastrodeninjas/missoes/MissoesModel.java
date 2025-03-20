663138917dbac65276af1a42b98b56c209e5a9fe
michael.aguiar <michael.aguiar@rpe.tech>
2025-03-19 23:53:41 -0400

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java10x/cadastrodeninjas/missoes/MissoesModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/missoes/MissoesModel.java
@@ -2,10 +2,16 @@ package dev.java10x.cadastrodeninjas.missoes;
 
 import dev.java10x.cadastrodeninjas.ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tb_missoes")
 public class MissoesModel {
 
--- src/main/java/dev/java10x/cadastrodeninjas/ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/ninjas/NinjaModel.java
@@ -3,8 +3,14 @@ package dev.java10x.cadastrodeninjas.ninjas;
 
 import dev.java10x.cadastrodeninjas.missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tb_cadastro")
 public class NinjaModel {
 
@@ -22,44 +28,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
