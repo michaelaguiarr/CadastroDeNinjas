984b079f4f5e2daacfeb2f04ae6f5a6b7cff2565
michael.aguiar <michael.aguiar@rpe.tech>
2025-03-21 10:12:47 -0400

    feat: Migrations para tb_cadastro incluindo a column de rank

--- src/main/java/dev/java10x/cadastrodeninjas/ninjas/model/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/ninjas/model/NinjaModel.java
@@ -33,6 +33,9 @@ public class NinjaModel {
     @Column(name = "idade")
     private int idade;
 
+    @Column(name = "rank")
+    private String rank;
+
     // @ManyToOne - Um Ninja tem uma unica missao
     @ManyToOne
     @JoinColumn(name = "missoes_id")
